0f541fd004fae9b6d35ace9fdb7eb8e0b6412e1b
dong <ldr113@naver.com>
2022-01-21 16:12:03 +0900

    4 commit

---
+++ test/js_html연결/jsimport.js
@@ -0,0 +1,10 @@
+let color = prompt("색깔을 적어주세요(빨강, 초록, 파랑)")
+if(color == "빨강"){
+  document.body.style.background="red"
+}
+else if(color == "초록"){
+  document.body.style.background="green"
+}
+else if(color == "파랑"){
+  document.body.style.background="blue"
+}
